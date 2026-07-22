50d9c1b69f36ba44716aaf0a370e892f092f189c
Yangsy115 <seungyeobyang09@gmail.com>
2026-07-22 13:19:17 +0900

    Day 1일차: 배열값의 가장큰 수 두번째로 큰 수 구하기.
    Day 1: 배열 최댓값/두 번째 최댓값 함수 작성 + AI 검증

--- day1/main.c
+++ day1/main.c
@@ -5,24 +5,40 @@ int main() {
     int n;
     scanf("%d", &n);
     
-    if (n == NULL || 2 > n) {
+    while (n < 2) {
         printf("배열 생성에 실패했습니다. 2이상의 정수값을 입력해 다시시도해주세요.\n");
         scanf("%d", &n);
     }
     
     int *arr = malloc(sizeof(int) * n);
-    int max = arr[0];
-
-    arr[7] = 1;
-    arr[8] = 2;
-    arr[9] = 1;
-
     
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > max) { 
-            max = arr[i];   
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max1, max2;
+    if (arr[0] > arr[1]) {
+        max1 = arr[0];
+        max2 = arr[1];
+    } else {
+        max1 = arr[1];
+        max2 = arr[0];
+    }
+
+    for (int i = 2; i < n; i++) {
+        if (arr[i] >= max1) { 
+            max2 = max1;    
+            max1 = arr[i];  
+        } 
+        else if (arr[i] > max2) { 
+            max2 = arr[i];  
         }
     }
+    
+    printf("가장 큰 수: %d\n", max1);
+    printf("두 번째로 큰 수: %d\n", max2);
+
+    free(arr);
     
     return 0;
 }
